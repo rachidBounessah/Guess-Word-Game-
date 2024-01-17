adf22a069310c543f26327cd01e8d5d8fc7e3e87
rachid <rachid12br@gmail.com>
2024-01-17 22:58:48 +0100

    add style to game-area // add name to footer and h1

---
+++ guess.js
@@ -0,0 +1,8 @@
+//Setting Game Name
+
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector(
+  "footer"
+).innerHTML = `${gameName} Game Created By Zero Web Shcool`;
