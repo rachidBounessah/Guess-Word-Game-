af8c65777b092a840772ba3ba2c6cc002870660c
rachid <rachid12br@gmail.com>
2024-01-17 23:54:41 +0100

    Generate Inputs

--- guess.js
+++ guess.js
@@ -6,3 +6,37 @@ document.querySelector("h1").innerHTML = gameName;
 document.querySelector(
   "footer"
 ).innerHTML = `${gameName} Game Created By Zero Web Shcool`;
+
+//Setting Game Options
+let numbersOfTries = 6;
+let numbersOfLetters = 6;
+let currentTry = 1;
+
+function generateInput() {
+  const inputContainer = document.querySelector(".inputs");
+  //create main tryDiv
+  for (let i = 1; i <= numbersOfTries; i++) {
+    const tryDiv = document.createElement("div");
+    tryDiv.classList.add(`try-${i}`);
+    tryDiv.innerHTML = `<span>Try ${i}</span> `;
+
+    if (i !== 1) tryDiv.classList.add("disabled-inputs");
+    //create input
+    for (j = 1; j <= numbersOfLetters; j++) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.classList.add(`${j}`);
+      input.id = `guess-${i}-letter-${j}`;
+      input.setAttribute("maxlength", "1");
+      tryDiv.appendChild(input);
+    }
+
+    inputContainer.appendChild(tryDiv);
+  }
+
+  inputContainer.children[0].children[1].focus();
+}
+
+window.onload = () => {
+  generateInput();
+};
